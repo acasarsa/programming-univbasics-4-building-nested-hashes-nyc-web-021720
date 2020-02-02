547c7556cd4a62099dc410fe6652df0d43f19065
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-02 00:59:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,10 @@
 def base_hash
-	{key: value}
+	{key: "ball"}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
+	{key: "ball"}
+	# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
