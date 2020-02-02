09d69e4aae296d48fdf12461adcf6a21f636b81b
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-02 01:22:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ end
 def monopoly_with_third_tier
 	{:railroads => {
 		:pieces => 4,
-		:rent_in_dollars => {one_piece_owned: 25, b: 2, c: 3, d: 4},
+		:rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 75, four_pieces_owned: 100},
 		:names => {a: 1, b: 2, c: 3, d: 4}
 	}
 }
